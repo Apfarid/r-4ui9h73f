08fcab85e0fe44b9c5c4d6997fc842116dc85903
apfarid <apfarid780@gmail.com>
2019-09-07 15:21:53 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,31 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  state = {
+    caja : 'My new value'
+  }
+
+  handleChance = e => (
+    this.setState({
+      caja : e.target.value
+      
+    })
+  )
+
+
   render() {
+
+
+
+
     return (
       <div>
         <h1>Un repetidor</h1>
-        <input type="text" placeholder="Empieza a escribir algo" />
-        <p className="repeater"></p>
+        <input type="text" placeholder="Empieza a escribir algo" 
+          onChange={this.handleChance}
+          value = {this.state.caja}
+        />
+        <p className="repeater">{this.state.caja}</p>
       </div>
     );
   }
